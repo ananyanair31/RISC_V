70e72e8969790a2e2052bec9e0f63b609e0fc429
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2025-03-12 18:01:14 +0530

    Update src.v

--- ALU/src.v
+++ ALU/src.v
@@ -3,16 +3,10 @@
 module alu(
     input [7:0] a,b,
     input [3:0] alu_sel,
-    output [7:0] alu_out,
-    output zero
+    output reg [7:0] alu_result,
+    output reg zero
     );
-    
-    reg [7:0] alu_result;
-    wire [8:0] tmp;
-    assign alu_out = alu_result;
-    assign tmp = {1'b0,a} + {1'b0,b};
-    assign carryout = tmp[8];
-    
+        
     always @(*)
     begin
         case(alu_sel)
@@ -32,9 +26,10 @@ module alu(
         4'b1100: alu_result = {a[6:0],a[7]};
         4'b1101: alu_result = {a[0],a[7:1]};
         4'b1110: alu_result = (a==b)? 8'd1 : 8'd0;
+	    4'b1111: alu_result = (a>b)?8'd1:8'd0 ;
         
         default: alu_result = a+b;
-        
         endcase 
+        zero = (alu_result == 8'b0) ? 1'b1 : 1'b0;
      end
 endmodule

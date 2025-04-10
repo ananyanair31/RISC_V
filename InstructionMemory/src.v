5a1146f20b004ba8c1fda289eaaa8dcf768cd64d
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2025-04-10 19:24:54 +0530

    Update src.v

--- InstructionMemory/src.v
+++ InstructionMemory/src.v
@@ -1,35 +1,24 @@
 `timescale 1ns / 1ps
 
-module iMem(
-    input [3:0] addr,  // PC addr
-    output reg [15:0] instruction
+module instructionMem(
+    input [31:0] pc,
+    output wire [31:0] instruction
     );
-    
-    reg [15:0] mem [0:15];  
 
+    reg [31:0] memory [0:255];
+
+    // Simulation-only code
+    `ifndef SYNTHESIS
     initial begin
-    
-    // initialize memory with instructions for the alu
-    mem[0] = 16'b0000000000000000;
-    mem[1] = 16'b0000000000000001;
-    mem[2] = 16'b0000000000000010;
-    mem[3] = 16'b0000000000000011;
-    mem[4] = 16'b0000000000000100;
-    mem[5] = 16'b0000000000000101;
-    mem[6] = 16'b0000000000000110;
-    mem[7] = 16'b0000000000000111;
-    mem[8] = 16'b0000000000001000;
-    mem[9] = 16'b0000000000001001;
-    mem[10] = 16'b0000000000001010;
-    mem[11] = 16'b0000000000001011;
-    mem[12] = 16'b0000000000001100;
-    mem[13] = 16'b0000000000001101;
-    mem[14] = 16'b0000000000001110;
-    mem[15] = 16'b0000000000001111;
+        $display(">> Instruction Memory Initialized");
+        $readmemh("instructions.mem", memory);
     end
+    `endif
     
-    always @(addr)
-    begin
-        instruction = mem[addr];
-    end
+    initial begin
+        memory[0] = 32'hDEADBEEF;
+        memory[1] = 32'hCAFEBABE;
+    end 
+
+    assign instruction = memory[pc >> 2];
 endmodule

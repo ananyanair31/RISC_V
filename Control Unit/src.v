2fe470582d719ee2c4c5e06e134a092ee308d8d3
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2025-04-11 11:35:48 +0530

    Update src.v

--- Control Unit/src.v
+++ Control Unit/src.v
@@ -1,31 +1,100 @@
 `timescale 1ns / 1ps
 
+// Generates aluOp and other control signals using the opcode part of the instruction signal
 module controlUnit(
-    input [3:0] opcode,
-    output reg cIn, Sh_LR, rotEn, cmpEn,
-    output reg [3:0] aluOp
+    input [6:0] opcode,     // from inst decode
+    output reg [1:0] aluOp, // goes to alu control unit
+    output reg regWrite,    // goes to reg file
+    output reg aluSrc,
+    output reg memRead,     // goes to data mem
+    output reg memWrite,
+    output reg memToReg,    // write from data mem to reg file
+    output reg branch,      // goes to pc
+    output reg jump    
     );
     
-    always @(*)
-    begin
-        // Default Values
-        cIn = 0;
-        Sh_LR = 0;
-        rotEn = 0;
-        cmpEn = 0;
-        aluOp = opcode;
-        
+    always @(*) begin
         case (opcode)
-            4'b0000: cIn = 1; // ADD
-            4'b0001: cIn = 1; //SUB
-            4'b1000: Sh_LR = 0; // shift left
-            4'b1001: Sh_LR = 1; // shift Right
-            4'b1010: Sh_LR = 0; // Arithmetic Shift Left
-            4'b1011: Sh_LR = 1; // Arithmetic Shift Right
-            4'b1100: rotEn = 1; // Rotate Left
-            4'b1101: rotEn = 1; // Rotate Right
-            4'b1110: cmpEn = 1; // Compare
-            default: ; // Other operations use default values
+            7'b0110011: begin   // R type
+                aluOp = 2'b10;
+                regWrite = 1;
+                aluSrc = 0;
+                memWrite = 0;
+                memRead = 0;
+                memToReg = 0;
+                branch = 0;
+                jump = 0;
+            end           
+             7'b0010011: begin   // I type (addi)
+                aluOp = 2'b00;
+                regWrite = 1;
+                aluSrc = 1;
+                memWrite = 0;
+                memRead = 0;
+                memToReg = 0;
+                branch = 0;
+                jump = 0;
+            end
+            7'b0000011: begin   // Load (lw)
+                aluOp = 2'b00;
+                regWrite = 1;
+                aluSrc = 1;
+                memWrite = 0;
+                memRead = 1;
+                memToReg = 1;
+                branch = 0;
+                jump = 0;
+            end
+            7'b0100011: begin   // Store (sw)
+                aluOp = 2'b00;
+                regWrite = 0;
+                aluSrc = 1;
+                memWrite = 1;
+                memRead = 0;
+                memToReg = 0;
+                branch = 0;
+                jump = 0;
+            end
+            7'b1100011: begin   // Branch (beq)
+                aluOp = 2'b01;
+                regWrite = 0;
+                aluSrc = 0;
+                memWrite = 0;
+                memRead = 0;
+                memToReg = 0;
+                branch = 1;
+                jump = 0;
+            end
+            7'b1101111: begin   // JAL
+                aluOp = 2'b00;  // dont care
+                regWrite = 1;
+                aluSrc = 0;
+                memWrite = 0;
+                memRead = 0;
+                memToReg = 0;   // dont care
+                branch = 0;
+                jump = 1;
+            end
+            7'b1100111: begin   // JALR
+                aluOp = 2'b00;
+                regWrite = 1;
+                aluSrc = 1;
+                memWrite = 0;
+                memRead = 0;
+                memToReg = 0;
+                branch = 0;
+                jump = 1;
+            end
+            default: begin   
+                aluOp = 2'b00;
+                regWrite = 0;
+                aluSrc = 0;
+                memWrite = 0;
+                memRead = 0;
+                memToReg = 0;
+                branch = 0;
+                jump = 0;
+            end
         endcase
     end
 endmodule

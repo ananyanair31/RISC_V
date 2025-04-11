1069814a4ba3c5370be1c5428bab5c12573fa3ee
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2025-04-11 11:38:42 +0530

    Update tb.v

--- tb.v
+++ tb.v
@@ -4,19 +4,14 @@ module tb;
     reg clk, reset;
     reg [31:0] branch_tgt;
     reg [31:0] jump_tgt;
-    reg branch_taken;
-    reg jump;
-    reg regWrite;
+//    reg branch_taken;
+//    reg jump;
+//    reg regWrite;
     reg [31:0] writeData;
-
-    wire [6:0] opcode;
-    wire [4:0] rd;
-    wire [2:0] funct3;
-    wire [4:0] rs1;
-    wire [4:0] rs2;
-    wire [6:0] funct7;
     wire [31:0] readData1;
     wire [31:0] readData2;
+    wire aluSrc;
+    wire [3:0] aluControl;
 
     // Instantiate the top module
     topModule uut (
@@ -24,18 +19,14 @@ module tb;
         .reset(reset),
         .branch_tgt(branch_tgt),
         .jump_tgt(jump_tgt),
-        .branch_taken(branch_taken),
-        .jump(jump),
-        .regWrite(regWrite),
+//        .branch_taken(branch_taken),
+//        .jump(jump),
+//        .regWrite(regWrite),
         .writeData(writeData),
-        .opcode(opcode),
-        .rd(rd),
-        .funct3(funct3),
-        .rs1(rs1),
-        .rs2(rs2),
-        .funct7(funct7),
         .readData1(readData1),
-        .readData2(readData2)
+        .readData2(readData2),
+        .aluSrc(aluSrc),
+        .aluControl(aluControl)
     );
 
     // Clock Generation
@@ -45,41 +36,42 @@ module tb;
         // Initialize
         clk = 0;
         reset = 1;
-        branch_taken = 0;
-        jump = 0;
+//        branch_taken = 0;
+//        jump = 0;
         branch_tgt = 0;
         jump_tgt = 0;
-        regWrite = 0;
+//        regWrite = 0;
         writeData = 32'h0;
 
         #10 reset = 0; // Deassert reset
 
         // Write test value to register 3
         @(posedge clk);
-        regWrite = 1;
+//        regWrite = 1;
         writeData = 32'hABCD11;  // test pattern
 
-        @(posedge clk);
-        regWrite = 0;
+//        @(posedge clk);
+//        regWrite = 0;
 
-        // Read from registers rs1/rs2 (assuming rs1=3 in instruction.mem)
-        #20;
+//        // Read from registers rs1/rs2 (assuming rs1=3 in instruction.mem)
+        #50;
 
         // Check if branch is taken
         @(posedge clk);
-        branch_taken = 1;
+//        branch_taken = 1;
         branch_tgt = 32'h00000008;
 
         @(posedge clk);
-        branch_taken = 0;
-
+//        branch_taken = 0;
+        branch_tgt = 32'h0;
+        
         // Check if the program jumps
         @(posedge clk);
-        jump = 1;
+//        jump = 1;
         jump_tgt = 32'h0000000C;
 
         @(posedge clk);
-        jump = 0;
+        jump_tgt = 32'h0;
 
         #40; // Wait to allow PC to progress
 

a3648a0735df28277221f3a6a3af7dfe5848c482
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2025-04-11 11:37:40 +0530

    Update topModule.v

--- topModule.v
+++ topModule.v
@@ -5,22 +5,32 @@ module topModule(
     input reset,
     input [31:0] branch_tgt,
     input [31:0] jump_tgt,
-    input branch_taken,
-    input jump,  
-    input regWrite,
     input [31:0] writeData,
-    output wire [6:0] opcode,
-    output wire [4:0] rd,
-    output wire [2:0] funct3,
-    output wire [4:0] rs1,
-    output wire [4:0] rs2,
-    output wire [6:0] funct7,
     output wire [31:0] readData1,
-    output wire [31:0] readData2    
+    output wire [31:0] readData2,  
+    output wire aluSrc,  
+    output wire [3:0] aluControl
     );
-        
+       
+    // pc and instr
     wire [31:0] pc;
     wire [31:0] instruction;
+    wire branch_taken;
+    wire jump;
+    wire regWrite;
+    
+    // instr decode
+    wire [6:0] opcode;
+    wire [4:0] rd;
+    wire [2:0] funct3;
+    wire [4:0] rs1;
+    wire [4:0] rs2;
+    wire [6:0] funct7;
+    
+    // control unit
+    wire [1:0] aluOp;
+    wire memRead, memWrite, memToReg;
+//    wire [3:0] aluControl;
     
     pc pc_inst (
         .clk(clk),
@@ -57,5 +67,24 @@ module topModule(
         .writeData(writeData),
         .readData1(readData1), 
         .readData2(readData2)
+    );
+    
+    controlUnit cu_inst (
+        .opcode(opcode),     
+        .aluOp(aluOp), 
+        .regWrite(regWrite),    
+        .aluSrc(aluSrc),
+        .memRead(memRead),     
+        .memWrite(memWrite),
+        .memToReg(memToReg),    
+        .branch(branch_taken),      
+        .jump(jump)    
+    );
+    
+    aluControl ac_inst (
+        .aluOp(aluOp),
+        .funct3(funct3),
+        .funct7(funct7),
+        .aluControl(aluControl)
     );
 endmodule

fe58d286fcadb88a0515876ee7bad7a7cb8411e3
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2025-04-15 16:12:04 +0530

    Update src.v

--- PC/src.v
+++ PC/src.v
@@ -3,22 +3,24 @@
 module pc(
     input clk, 
     input reset,
-    input [31:0] pc,
     input [31:0] branch_tgt,
     input [31:0] jump_tgt,
     input branch_taken,
     input jump,
-    output reg [31:0] next_pc
+    input pcWrite,
+    output reg [31:0] pc
     );
     
     always @(posedge clk or posedge reset) begin
         if (reset)
-            next_pc <= 32'h00000000;
-        else if (jump)
-            next_pc <= jump_tgt;
-        else if (branch_taken) 
-            next_pc <= branch_tgt;
-        else 
-            next_pc <= pc + 4;
+            pc <= 32'h00000000;
+        else if (pcWrite) begin
+            if (jump)
+                pc <= jump_tgt;
+            else if (branch_taken) 
+                pc <= branch_tgt;
+            else 
+                pc <= pc + 4;
+        end
     end
 endmodule
